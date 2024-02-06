c468da2be0b550ab3ad8296caa73195e4fdbc7e5
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-06 09:22:29 -0300

    feat: replaced User for common.User type

--- auth/routes.go
+++ auth/routes.go
@@ -4,16 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/EzequielBPullolil/auth_service/common"
 )
 
-type User struct {
-	Name, Email, Password string
-}
-
-func HandleAuthRoutes(s *http.ServeMux) {
+func HandleAuthRoutes(s *http.ServeMux, db_inyection common.Repository) {
 	s.Handle("/auth/singup", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" || r.Method == "post" {
-			var u User
+			var u common.User
 			json.NewDecoder(r.Body).Decode(&u)
 			response := fmt.Sprintf(`{
 				"status": "Successful user registration",
@@ -30,7 +28,7 @@ func HandleAuthRoutes(s *http.ServeMux) {
 
 	s.Handle("/auth/login", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" || r.Method == "post" {
-			var u User
+			var u common.User
 			json.NewDecoder(r.Body).Decode(&u)
 			response := fmt.Sprintf(`{
 				"status": "Successful user login",
