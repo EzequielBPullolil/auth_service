d70d90fe1f3926a7d55b01dc4bc2767f56588600
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-07 23:32:19 -0300

    refact: Moved all logic from router users to controller

--- users/controller.go
+++ users/controller.go
@@ -1,6 +1,15 @@
 package users
 
-import "github.com/EzequielBPullolil/auth_service/common"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"strings"
+
+	"github.com/EzequielBPullolil/auth_service/common"
+)
 
 type UserController struct {
 	repo common.Repository
@@ -9,5 +18,65 @@ type UserController struct {
 func NewUserController(db_repository common.Repository) UserController {
 	return UserController{
 		repo: db_repository,
+	}
+}
+
+func (uc UserController) GetAuthenticatedUser(res http.ResponseWriter, req *http.Request) {
+	user, _ := uc.repo.Read("as")
+	res.WriteHeader(200)
+	if _, err := res.Write([]byte(user.ToJson())); err != nil {
+		_, _, line, _ := runtime.Caller(0)
+		log.Printf("Error en la línea %d: %s\n", line, err.Error())
+	}
+}
+func (uc UserController) UpdateAuthenticatedUser(res http.ResponseWriter, req *http.Request) {
+	var u common.User
+	if err := json.NewDecoder(req.Body).Decode(&u); err != nil {
+		_, _, line, _ := runtime.Caller(0)
+		log.Fatalf("Error en la línea %d: %s\n", line, err.Error())
+	}
+	updated_user, _ := uc.repo.Update("fake_id", u)
+
+	response := fmt.Sprintf(`{
+				"status": "Successful user update",
+				"data": %s
+			}`, updated_user.ToJson())
+
+	if _, err := res.Write([]byte(response)); err != nil {
+		_, _, line, _ := runtime.Caller(0)
+		log.Printf("Error en la línea %d: %s\n", line, err.Error())
+	}
+}
+func (uc UserController) DeleteAuthenticatedUser(res http.ResponseWriter, req *http.Request) {
+	uc.repo.Delete("fake_id")
+
+	response := fmt.Sprintf(`{
+			"status": "Successful user delete",
+		}`)
+
+	if _, err := res.Write([]byte(response)); err != nil {
+		_, _, line, _ := runtime.Caller(0)
+		log.Printf("Error en la línea %d: %s\n", line, err.Error())
+	}
+}
+
+func (uc UserController) GetUserById(res http.ResponseWriter, req *http.Request) {
+	id := strings.TrimPrefix(req.URL.Path, "/users/")
+	user, _ := uc.repo.Read(id)
+	res.WriteHeader(200)
+	if _, err := res.Write([]byte(user.ToJson())); err != nil {
+		_, _, line, _ := runtime.Caller(0)
+		log.Printf("Error en la línea %d: %s\n", line, err.Error())
+	}
+}
+
+func (uc UserController) HandleMethod(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		uc.GetAuthenticatedUser(w, r)
+	case http.MethodPut:
+		uc.UpdateAuthenticatedUser(w, r)
+	case http.MethodDelete:
+		uc.DeleteAuthenticatedUser(w, r)
 	}
 }
--- users/routes.go
+++ users/routes.go
@@ -1,62 +1,13 @@
 package users
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"runtime"
-	"strings"
 
 	"github.com/EzequielBPullolil/auth_service/common"
 )
 
 func HandleUserRoute(s *http.ServeMux, db_inyection common.Repository) {
-	s.Handle("/users", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			user, _ := db_inyection.Read("as")
-			w.WriteHeader(200)
-			if _, err := w.Write([]byte(user.ToJson())); err != nil {
-				_, _, line, _ := runtime.Caller(0)
-				log.Printf("Error en la línea %d: %s\n", line, err.Error())
-			}
-		case http.MethodPut:
-			var u common.User
-			if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-				_, _, line, _ := runtime.Caller(0)
-				log.Fatalf("Error en la línea %d: %s\n", line, err.Error())
-			}
-			updated_user, _ := db_inyection.Update("fake_id", u)
-
-			response := fmt.Sprintf(`{
-				"status": "Successful user update",
-				"data": %s
-			}`, updated_user.ToJson())
-
-			if _, err := w.Write([]byte(response)); err != nil {
-				_, _, line, _ := runtime.Caller(0)
-				log.Printf("Error en la línea %d: %s\n", line, err.Error())
-			}
-		case http.MethodDelete:
-			db_inyection.Delete("fake_id")
-
-			response := fmt.Sprintf(`{
-				"status": "Successful user delete",
-			}`)
-			if _, err := w.Write([]byte(response)); err != nil {
-				_, _, line, _ := runtime.Caller(0)
-				log.Printf("Error en la línea %d: %s\n", line, err.Error())
-			}
-		}
-	}))
-	s.Handle("/users/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		id := strings.TrimPrefix(r.URL.Path, "/users/")
-		user, _ := db_inyection.Read(id)
-		w.WriteHeader(200)
-		if _, err := w.Write([]byte(user.ToJson())); err != nil {
-			_, _, line, _ := runtime.Caller(0)
-			log.Printf("Error en la línea %d: %s\n", line, err.Error())
-		}
-	}))
+	userController := NewUserController(db_inyection)
+	s.Handle("/users", http.HandlerFunc(userController.HandleMethod))
+	s.Handle("/users/", http.HandlerFunc(userController.GetUserById))
 }
