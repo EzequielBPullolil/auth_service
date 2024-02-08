882fbd49c722ebf03380ccc2464d2df3802957d2
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-07 23:08:37 -0300

    refact: Moved the entire Auth route log to the UserController

--- auth/controller.go
+++ auth/controller.go
@@ -34,3 +34,28 @@ func (uc UserController) SignupUser(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(response))
 	}
 }
+func (uc UserController) LoginUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" || r.Method == "post" {
+		var u common.User
+		json.NewDecoder(r.Body).Decode(&u)
+		user, _ := uc.repo.Read(u.GetId())
+		response := fmt.Sprintf(`{
+			"status": "Successful user login",
+			"data":{
+				"token": "%s",
+				"user": %s
+			}
+		}`, CreateToken(user.GetId()), user.ToJson())
+		w.WriteHeader(201)
+		w.Write([]byte(response))
+	}
+}
+
+func (uc UserController) ValidateUserToken(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" || r.Method == "post" {
+		w.WriteHeader(200)
+		w.Write([]byte(`{
+			"status": "Valid auth token",
+		}`))
+	}
+}
--- auth/routes.go
+++ auth/routes.go
@@ -1,8 +1,6 @@
 package auth
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/EzequielBPullolil/auth_service/common"
@@ -11,30 +9,6 @@ import (
 func HandleAuthRoutes(s *http.ServeMux, db_inyection common.Repository) {
 	user_controller := NewUserController(db_inyection)
 	s.Handle("/auth/singup", http.HandlerFunc(user_controller.SignupUser))
-
-	s.Handle("/auth/login", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" || r.Method == "post" {
-			var u common.User
-			json.NewDecoder(r.Body).Decode(&u)
-			user, _ := db_inyection.Read(u.GetId())
-			response := fmt.Sprintf(`{
-				"status": "Successful user login",
-				"data":{
-					"token": "%s",
-					"user": %s
-				}
-			}`, CreateToken(user.GetId()), user.ToJson())
-			w.WriteHeader(201)
-			w.Write([]byte(response))
-		}
-	}))
-
-	s.Handle("/auth/validate", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" || r.Method == "post" {
-			w.WriteHeader(200)
-			w.Write([]byte(`{
-				"status": "Valid auth token",
-			}`))
-		}
-	}))
+	s.Handle("/auth/login", http.HandlerFunc(user_controller.LoginUser))
+	s.Handle("/auth/validate", http.HandlerFunc(user_controller.ValidateUserToken))
 }
