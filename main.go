1dd9274497218e0b8e3928624c37484f9bc74d96
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-11 06:04:11 -0300

    feat: Setup routes in main

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/EzequielBPullolil/auth_service/users"
+	"github.com/EzequielBPullolil/auth_service/users/auth"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+
 	server := http.NewServeMux()
+	connectionPool, _ := pgxpool.New(context.Background(), os.Getenv("DB_URI"))
+	user_repo := users.NewUserRepository(connectionPool)
+	user_repo.CreateTables()
+	users.HandleUserRoute(server, user_repo)
+	auth.HandleAuthRoutes(server, user_repo)
 	log.Println("Server start at port 8030")
 	log.Fatal(http.ListenAndServe(":8030", server))
 }
