85a0287896c3f2ef59439a10fae3a9d25c03c229
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-15 18:28:57 -0300

    refact: Improved readability of the auth controller - Removed all else clauses - Improved error handling - Now all error responses use the ponseError method -

--- src/auth/controller.go
+++ src/auth/controller.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -27,78 +28,69 @@ func (uc AuthController) SignupUser(res http.ResponseWriter, r *http.Request) {
 	u := uc.GetUserData(r)
 
 	if err, ok := u.ValidateFields(); !ok {
-		response := fmt.Sprintf(`{
-			"status": "error signup user",
-			"error": "%s"
-		}`, err.Error())
-		uc.ResponseWithStatus(response, http.StatusBadRequest, res)
+		uc.ResponseError("error signup user", err, res)
 		return
 	}
-
-	if entity, err := uc.repo.Create(u); err == nil {
-		response := fmt.Sprintf(`{
+	entity, err := uc.repo.Create(u)
+	if err != nil {
+		uc.ResponseError("Error while persisting user", err, res)
+		return
+	}
+	response := fmt.Sprintf(`{
 			"status": "Successful user registration",
 			"data":{
 				%s
 			}
 		}`, entity.ToJson())
-		uc.ResponseWithStatus(response, http.StatusCreated, res)
-		log.Println(u, entity, err)
-	} else {
-		uc.ResponseWithStatus(fmt.Sprintf(`{
-			"error": "%s"
-		}`, err.Error()), http.StatusCreated, res)
-		log.Println(u, entity, err)
-	}
+	uc.ResponseJsonWithStatus(response, http.StatusCreated, res)
 }
 func (uc AuthController) LoginUser(res http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		u := uc.GetUserData(r)
-		if user, err := uc.repo.Read(u.GetEmail()); err == nil {
-			response := fmt.Sprintf(`{
-				"status": "Successful user login",
-				"data":{
-					"token": "%s",
-					"user": %s
-				}
-			}`, tokenmanager.CreateToken(user.GetEmail()), user.ToJson())
-			uc.ResponseWithStatus(response, http.StatusCreated, res)
-		} else {
-			uc.ResponseWithStatus(fmt.Sprintf(`{
-				"error": "%s"
-			}`, err.Error()), http.StatusBadRequest, res)
-		}
+	if r.Method != http.MethodPost {
+		return
 	}
+	u := uc.GetUserData(r)
+	user, err := uc.repo.Read(u.GetEmail())
+	if err != nil {
+		uc.ResponseError("Error while login user", err, res)
+		return
+	}
+
+	response := fmt.Sprintf(`{
+		"status": "Successful user login",
+		"data":{
+			"token": "%s",
+			"user": %s
+		}
+	}`, tokenmanager.CreateToken(user.GetEmail()), user.ToJson())
+	uc.ResponseJsonWithStatus(response, http.StatusCreated, res)
+
 }
 
 func (uc AuthController) ValidateUserToken(res http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		return
+	}
+	c, err := r.Cookie("auth_token")
 
-	if r.Method == http.MethodPost {
-		c, err := r.Cookie("auth_token")
+	if err != nil {
+		uc.ResponseError("Missing auth token", err, res)
+		return
+	}
 
-		if err != nil {
-			uc.ResponseWithStatus(`{
-				"status": "Invalid auth token",
-			}`, http.StatusBadRequest, res)
-			return
-		}
-		if id, ok := tokenmanager.ValidateToken(c.Value); ok {
-			log.Println(c, err, id, ok)
-			if id == "" {
-				uc.ResponseWithStatus(`{
-					"status": "Invalid auth token",
-				}`, http.StatusBadRequest, res)
-			} else {
-				uc.ResponseWithStatus(`{
-					"status": "Valid auth token",
-				}`, http.StatusOK, res)
-			}
-		} else {
-			uc.ResponseWithStatus(`{
-				"status": "Invalid auth token",
-			}`, http.StatusBadRequest, res)
-		}
-		// Si la cookie no está presente o el token no es válido
+	log.Println("\n" + c.Value)
+	if !tokenmanager.ValidateToken(c.Value) {
+		uc.ResponseError("Invalid auth token", errors.New(""), res)
+		return
+	}
+
+	id := tokenmanager.GetTokenId(c.Value)
+	if id == "" {
+		uc.ResponseError("Invalid auth token", errors.New(""), res)
+		return
 
 	}
+	uc.ResponseJsonWithStatus(`{
+				"status": "Valid auth token",
+			}`, http.StatusOK, res)
+
 }
