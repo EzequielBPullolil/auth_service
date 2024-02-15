d29b1067e04e129550ccb1091229c7e2482c962b
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-15 20:17:39 -0300

    (config) added flag '--log' to test script

--- test.sh
+++ test.sh
@@ -1,1 +1,25 @@
-gotestsum --watch --format pkgname --hide-summary=output --rerun-fails 
+#!/bin/bash
+
+# Definir las opciones y sus descripciones
+OPTIONS=$(getopt -o l --long log -- "$@")
+eval set -- "$OPTIONS"
+
+# Inicializar la variable para el formato
+format="pkgname"
+
+# Leer las opciones
+while true; do
+    case "$1" in
+    -l | --log)
+        format="testname"
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+
+# Ejecutar el comando gotestsum con las opciones
+gotestsum --watch --format $format --hide-summary=output --rerun-fails
